bb7adac3e5c6afc36cc613367c4a7c4fd64c2961
William Astorga <wastorga@cloverhound.com>
2023-03-27 19:21:06 -0600

    fix: remove deprecated set-output and ubuntu-18.04

--- scripts/build.sh
+++ scripts/build.sh
@@ -167,8 +167,10 @@ tar czf "${ROOT}/_build/${FILENAME}" "${APP}"
 # Set variables for GitHub Actions to pick up in subsequent steps
 if [ "$GITHUB_ACTIONS" == "true" ]; then
     log_debug "Setting GitHub Actions outputs"
-    echo ::set-output "name=path::_build/$FILENAME"
-    echo ::set-output "name=file::$FILENAME"
+    # echo ::set-output "name=path::_build/$FILENAME"
+    # echo ::set-output "name=file::$FILENAME"
+    echo "name=path::_build/$FILENAME" >> $GITHUB_OUTPUT
+    echo "name=file::$FILENAME" >> $GITHUB_OUTPUT
 fi
 
 log_debug "path: _build/$FILENAME"
